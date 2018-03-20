87b3cd4325d79ae79b55188792914ab658a2b9f6
krimson8 <krimson8@gmail.com>
2018-03-20 20:43:35 +0800

    added ans

--- UVa/UVA-10026.cpp
+++ UVa/UVA-10026.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 struct node
 {
-    int tf;
+    double tf;
     int num;
 }a[1000];
 
@@ -36,7 +36,7 @@ int main() {
         l = m;
         while(l--) {
             scanf("%d %d", &day, &fine);
-            a[i].tf = (float)fine / day;
+            a[i].tf = (double)fine / day;
             a[i].num = i + 1;
             // tf[i] = (float)fine / day;
             // printf("-- %f\n", tf[i]);
@@ -45,10 +45,13 @@ int main() {
         }
         
         stable_sort(a, a + i, cmp);
-        for(int k = 0; k < m; k++) {
-            printf("%d ", a[k].num);
+
+        printf("%d", a[0].num);
+        for(int k = 1; k < m; k++) {
+            printf(" %d", a[k].num);
             // printf("%d ", q[tf[k]]);
         }
         printf("\n");
+        if(n) printf("\n");
     }
 }
